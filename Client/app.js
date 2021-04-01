14a5551c0606b9efac4e08045edf73f6a61d5434
Almanac <ruoyw0@gmail.com>
2021-04-01 12:46:32 -0700

    Got basic retrive all movies and display them in a table to work

--- Client/app.js
+++ Client/app.js
@@ -14,6 +14,7 @@
             success: function( data, textStatus, jQxhr ){
                 for(var i=0;i < data.length; i++){
                     $('#response pre').append( $('<div>' + data[i].title + '</div>') );
+                    displayMovie(data[i]);
                 };
             },
             error: function( jqXhr, textStatus, errorThrown ){
@@ -26,3 +27,13 @@
 
     $('#my-form').submit( processForm );
 })(jQuery);
+
+function displayMovie(movieObject) {
+    var tbody = $('#movies-table tbody');
+    if (tbody.length >= 0) {
+        tbody.append(`<tr><td>${movieObject.movieId}</td><td>${movieObject.title}</td><td>${movieObject.director}</td><td>${movieObject.genre}</td></tr>`);
+    } else {
+        $('#movies-table').append('<tr><td>value</tr></td>');
+    }
+}
+
