cd8440724e0d1a2151f38fd5b5fe39fc49e6713c
Lewis Vine <lew.vine@gmail.com>
2021-04-01 15:18:19 -0400

    Tested display all functionality with GET and jQuery append method

--- Client/app.js
+++ Client/app.js
@@ -1,18 +1,20 @@
 (function($){
     function processForm( e ){
         var dict = {
-        	Title : this["title"].value,
+        	Title : this["title"].value, //Find MovieId where title = this;
         	Director: this["director"].value
         };
 
         $.ajax({
-            url: 'https://localhost:44325/api/movie',
+            url: 'https://localhost:44325/api/movie/',
             dataType: 'json',
-            type: 'post',
+            type: 'get',
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
+                for(var i=0;i < data.length; i++){
+                    $('#response pre').append( $('<div>' + data[i].title + '</div>') );
+                };
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
