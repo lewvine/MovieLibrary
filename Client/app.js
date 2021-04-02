552213b54f166aec7f19e4aae3173fcee5b8cfbd
Lewis Vine <lew.vine@gmail.com>
2021-04-02 14:38:11 -0400

    Added display functionality and open and close feature

--- Client/app.js
+++ Client/app.js
@@ -2,16 +2,18 @@
 
 (function($){
     $('#my-form').submit( processPutMovie );
+    homePage();
+
 })(jQuery);
 
-function displayMovie(movieObject) {
-    var tbody = $('#movies-table tbody');
-    if (tbody.length >= 0) {
-        tbody.append(`<tr class="movie-item"><td>${movieObject.movieId}</td><td>${movieObject.title}</td><td>${movieObject.director}</td><td>${movieObject.genre}</td></tr>`);
-    } else {
-        $('#movies-table').append('<tr><td>value</tr></td>');
-    }
-}
+// function displayMovie(movieObject) {
+//     var tbody = $('#movies-table tbody');
+//     if (tbody.length >= 0) {
+//         tbody.append(`<tr class="movie-item"><td>${movieObject.movieId}</td><td>${movieObject.title}</td><td>${movieObject.director}</td><td>${movieObject.genre}</td></tr>`);
+//     } else {
+//         $('#movies-table').append('<tr><td>value</tr></td>');
+//     }
+// }
 
 function homePage() {
     $("#my-form").hide();
@@ -25,7 +27,7 @@ function homePage() {
             $('#main-area').append($("<div class='row'>Movie</div>"));
             for(let i=0;i < data.length; i++){
                 $('#main-area').append($("<div class='row'>"));
-                $('#main-area').append( $(`<div id = movieId ${data[i].movieId} >${data[i].title}</div>`));
+                $('#main-area').append( $(`<div class="closed" onclick=displayMovie(${data[i].movieId}) id = movieId${data[i].movieId} >${data[i].title}</div>`));
                 $('#main-area').append($("</div>"));           
             };
         },
@@ -49,7 +51,7 @@ function processPutMovie( e ){
         contentType: 'application/json',
         data: JSON.stringify(dict),
         success: function( data, textStatus, jQxhr ){
-            $('#response pre').append( $('<div id = movieId' + data.movieId + '>' + data.title + '</div>') );
+            $('#response pre').append( $('<div class="open" id = movieId' + data.movieId + '>' + data.title + '</div>') );
         },
         error: function( jqXhr, textStatus, errorThrown ){
             console.log( errorThrown );
@@ -57,6 +59,32 @@ function processPutMovie( e ){
     });
 
     e.preventDefault();
+}
+
+function displayMovie(id){  
+    $.ajax({
+        url: `https://localhost:44325/api/movie/${id}`,
+        dataType: 'json',
+        type: 'Get',
+        contentType: 'application/json',
+        success: function( data, textStatus, jQxhr ){
+            if( $(`#movieId${id}`).hasClass("open"))
+            {
+                $(`#movieId${id}`).removeClass("open")
+                $(`#movieId${id}`).addClass("closed")
+                $(`#movieId${id}`).html(`${data.title}`);
+            }else{
+                $(`#movieId${id}`).removeClass("closed");
+                $(`#movieId${id}`).addClass("open");
+                $(`#movieId${id}`).empty();
+                $(`#movieId${id}`).html(`Movie Id: ${data.movieId}, Title:${data.title}, Genre:${data.genre}</div>`);         
+            }
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
+    
 }
 
 function addAMovie() {
