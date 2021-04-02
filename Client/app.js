39fb24cde846d68095ccf7c75b9b382b61803492
Lewis Vine <lew.vine@gmail.com>
2021-04-02 16:40:28 -0400

    Added delete functionality

--- Client/app.js
+++ Client/app.js
@@ -27,7 +27,7 @@ function homePage() {
             $('#main-area').append($("<div class='row'>Movie</div>"));
             for(let i=0;i < data.length; i++){
                 $('#main-area').append($("<div class='row'>"));
-                $('#main-area').append( $(`<div data-title="${data[i].title}" data-genre="${data[i].genre}" data-director="${data[i].director}" class="closed" onclick=displayMovie(${data[i].movieId}) id = movieId${data[i].movieId} >${data[i].title}</div>`));
+                $('#main-area').append( $(`<div data-title="${data[i].title}" data-genre="${data[i].genre}" data-director="${data[i].director}" class="closed" onclick=displayMovie(${data[i].movieId}) id = movieId${data[i].movieId} >${data[i].title}<button onclick='deleteMovie(${data[i].movieId})' class="btn btn-warning">Delete</button></div>`));
                 $('#main-area').append($("</div>"));
             };
             return data;           
@@ -63,20 +63,38 @@ function processPutMovie( e ){
 }
 
 function displayMovie(id){  
-    var element = document.Find(`#movieId${id}`);
+    var element = document.querySelector(`#movieId${id}`);
     var title = element.dataset.title;
     var genre = element.dataset.genre;
     var director = element.dataset.director;
 
-    if( element.hasClass("open"))
+    if( element.className == "open")
     {
-        element.removeClass("open").addClass("closed").html(`Title: ${title}, Genre: ${genre}, Director: ${director}`);
+        element.className = "closed";
+        element.innerHTML = `Title: ${title}, Genre: ${genre}, Director: ${director}`;
     }else{
-        element.removeClass("closed").addClass("open").html(`Title: ${title}`);     
+        element.className = "open";
+        element.innerHTML = `${title}`;     
     };
 }
 
 function addAMovie() {
     $('#main-area').empty();
     $("#my-form").show();
+}
+
+function deleteMovie(id){
+    $.ajax({
+        url: `https://localhost:44325/api/movie/${id}`,
+        dataType: 'json',
+        type: 'Delete',
+        contentType: 'application/json',
+        success: function( data, textStatus, jQxhr ){
+            console.log("This movie was successfully delete");
+            homePage();
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
 }
