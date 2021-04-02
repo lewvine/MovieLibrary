7da0ea7e8b095e871204a943351ec8339c214ada
Almanac <ruoyw0@gmail.com>
2021-04-02 09:07:23 -0700

    Switched from table to Container with rows for Movie titles on home page

--- Client/app.js
+++ Client/app.js
@@ -1,33 +1,7 @@
+"use strict";
+
 (function($){
-    function processForm( e ){
-        var dict = {
-        	Title : this["title"].value, //Find MovieId where title = this;
-        	Director: this["director"].value
-        };
-
-        $.ajax({
-            url: 'https://localhost:44325/api/movie/',
-            dataType: 'json',
-            type: 'get',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').empty();
-                $('.movie-item').empty();
-                for(let i=0;i < data.length; i++){
-                    $('#response pre').append( $('<div id = movieId' + data[i].movieId + '>' + data[i].title + '</div>') );            
-                    displayMovie(data[i]);
-                };
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
-
-        e.preventDefault();
-    }
-
-    $('#my-form').submit( processForm );
+    $('#my-form').submit( processPutMovie );
 })(jQuery);
 
 function displayMovie(movieObject) {
@@ -37,5 +11,50 @@ function displayMovie(movieObject) {
     } else {
         $('#movies-table').append('<tr><td>value</tr></td>');
     }
+}
+
+function homePage() {
+    $.ajax({
+        url: 'https://localhost:44325/api/movie/',
+        dataType: 'json',
+        type: 'Get',
+        contentType: 'application/json',
+        success: function( data, textStatus, jQxhr ){
+            $('#main-area').empty();
+            $('#main-area').append($("<div class='row'>Movie</div>"));
+            for(let i=0;i < data.length; i++){
+                $('#main-area').append($("<div class='row'>"));
+                $('#main-area').append( $(`<div id = movieId ${data[i].movieId} >${data[i].title}</div>`));
+                $('#main-area').append($("</div>"));           
+            };
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
+}
+
+function processPutMovie( e ){
+    var dict = {
+        Title : this["title"].value, //Find MovieId where title = this;
+        Director: this["director"].value,
+        Genre: this["genre"].value
+    };
+
+    $.ajax({
+        url: 'https://localhost:44325/api/movie/',
+        dataType: 'json',
+        type: 'Post',
+        contentType: 'application/json',
+        data: JSON.stringify(dict),
+        success: function( data, textStatus, jQxhr ){
+            $('#response pre').append( $('<div id = movieId' + data.movieId + '>' + data.title + '</div>') );
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
+
+    e.preventDefault();
 }
 
